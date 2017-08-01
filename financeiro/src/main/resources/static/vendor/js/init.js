4ec2ef298a9cacaf67e80fb5cb5ddf2ec96dd0ca
prmorais13 <prmorais_13@hotmail.com>
2017-08-01 01:16:35 -0300

    Sexto commit - Casa

--- financeiro/src/main/resources/static/vendor/js/init.js
+++ financeiro/src/main/resources/static/vendor/js/init.js
@@ -10,3 +10,9 @@
 $(document).ready(function() {
     $('select').material_select();
 });
+
+//init modal
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
